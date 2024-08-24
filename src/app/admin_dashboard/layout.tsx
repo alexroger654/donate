7b4bf6e1b1eb5afd6c1217f99ce4a9de7ca9d606
myKollege <mykollegeindia@gmail.com>
2024-08-24 14:55:58 +0600

    logout button added

--- src/app/admin_dashboard/layout.tsx
+++ src/app/admin_dashboard/layout.tsx
@@ -1,10 +1,16 @@
 "use client";
 import Loading from "@/components/Loading";
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useLayoutEffect, useState } from "react";
+import { useSession, signOut } from "next-auth/react";
+
+
+
+
+
+
 export default function UserLayout({
   children,
 }: Readonly<{
@@ -27,7 +33,9 @@ export default function UserLayout({
   const toggleProfileMenu = () => setIsProfileMenuOpen(!isProfileMenuOpen);
   const closeProfileMenu = () => setIsProfileMenuOpen(false);
 
-  console.log(session, "session");
+  const handleLogout = () => {
+    signOut({ callbackUrl: "/sign_in" });
+  };
 
   useLayoutEffect(() => {
     if (status !== "loading" && session?.user?.role !== "admin") {
@@ -271,8 +279,37 @@ export default function UserLayout({
 
                     <span className="mx-2 text-sm font-medium">Category</span>
                   </Link>
-                </div>
+
+                </div >
               </nav>
+              <button
+
+                onClick={handleLogout}
+                className="flex items-center justify-center px-3 py-3 text-white bg-red-600 transition-colors duration-300 transform rounded-lg "
+
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+
+                <span className="mx-2 text-sm font-medium">Log Out</span>
+              </button>
             </div>
           </div>
         </aside>
@@ -283,9 +320,8 @@ export default function UserLayout({
           <div className="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
         )}
         <aside
-          className={`fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-gray-900 dark:bg-gray-800 md:hidden ${
-            isSideMenuOpen ? "block" : ""
-          }`}
+          className={`fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-gray-900 dark:bg-gray-800 md:hidden ${isSideMenuOpen ? "block" : ""
+            }`}
         >
           <div className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
             <Link href="/admin_dashboard/">
